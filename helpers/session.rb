6be0f2b0c07894e4103e1f71a1c9051500f895b0
Katherine Guenther <guenther@peak.org>
2017-01-09 17:21:20 -0800

    Update session.rb

--- helpers/session.rb
+++ helpers/session.rb
@@ -8,9 +8,9 @@ helpers do
 
   def current_user
     if session[:user_id]
-      @user ||= User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     else
-      @user = nil
+      @current_user = nil
     end
   end
 
